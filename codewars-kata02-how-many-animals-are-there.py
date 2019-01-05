d416feb23523d979bb5907b153bc99fa848934af
adamrajcarless <46345887+adamrajcarless@users.noreply.github.com>
2019-01-05 23:48:24 +0000

    Update codewars-kata02-how-many-animals-are-there.py

--- codewars-kata02-how-many-animals-are-there.py
+++ codewars-kata02-how-many-animals-are-there.py
@@ -6,7 +6,8 @@ For example :
 """
 
 """
-my difficulty here is to consider multiple consequetive integers when iterating over the string. I've used groupby to do this for now
+my difficulty here is to consider multiple consequetive integers when iterating over the string. I googled groupby and pasted 
+together some solutions to do this for now. I don't fully understand it"
 """
 
 from itertools import groupby
@@ -19,3 +20,24 @@ s = [int(''.join(i))
     ]
     
 print(sum(s))
+
+"""
+This was as far as I got on my own...
+
+from itertools import groupby
+
+def numbers(n):
+    count = []
+    for i in n:
+        if i.isdigit():  #see what arguments this can take maybe to make whole numbers not single numbers
+            count += i
+    count = [int(i) for i in count] # turn the list from strings into int
+    return sum(count)
+
+print(numbers("there are 5 hippos, 50 beetles, 4 birds"))
+
+
+
+# 2 problems. 1) how to add up shit in an array. 2) how to consider a number as multiple digits in a for loop
+
+"""
